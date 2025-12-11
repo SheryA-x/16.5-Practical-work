e4c9e37ac1b1880ad2dcb324084d15e3d813c890
Sherya <DjVooLF@yandex.ru>
2025-12-11 11:06:22 +0400

    Выполнено

--- Source.cpp
+++ Source.cpp
@@ -10,37 +10,35 @@ int main()
 	time_t t = time(NULL);
 	localtime_s(&buf, &t);
 
-
+	int sum = 0;
 	const int size = 5;
 	int array[size][size];
-	
 	int index = buf.tm_mday % size;
 	
-	int sum=0;
-
 	for (int i = 0; i < size; i++)
 	{
-
 		for (int j = 0; j < size; j++)
 		{
 			array[i][j] = i + j;
 			cout << array[i][j] << " ";
+			
+			if (i == index)
+			{
+				sum = array[index][j] + sum;
+			}
 		}
+		if (i == index)
+		{
+			cout << "—умма строки составл€ет = " << sum;
+		}
+
 		cout << endl;
+		
 	}
-
-	for (int i = 0; i < size; i++)
-	{
-		sum = array[index][i] + sum;
-	}
-
-
-
+	cout << endl;
+	cout << "N = " << size << endl;
 	cout << "ƒеление сегодн€шнего дн€ на N = " << index << endl;
-	cout << "—умма строки составл€ет = " << sum << endl;
 
-
-	//¬ыведите сумму элементов в строке массива, индекс которой равен остатку делени€ текущего числа календар€ на N(в двумерном массиве a[i][j], i Ч индекс строки).
 
 	
 
